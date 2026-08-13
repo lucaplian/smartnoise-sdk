e4df4c1821cad422d4a551f8a1b2199eb468d420
Luca Plian <98339220+AnonimAnonim2245@users.noreply.github.com>
2026-08-13 11:16:16 +0300

    fixes

--- synth/snsynth/pytorch/nn/dpctgan.py
+++ synth/snsynth/pytorch/nn/dpctgan.py
@@ -369,20 +369,21 @@ class DPCTGAN(CTGANSynthesizer, Synthesizer):
                     fake_cat = fakeact
 
                 optimizerD.zero_grad()
-                print("self=", self)
-                print("self.loss=", self.loss)
+                if i >=self._epochs-10:
+                    print("self=", self)
+                    print("self.loss=", self.loss)
                 if self.loss == "cross_entropy":
                     y_fake = discriminator(fake_cat)
-
-                    print ('y_fake is {}'.format(y_fake))
+                    if i >=self._epochs-10:
+                        print ('y_fake is {}'.format(y_fake))
                     label_fake = torch.full(
                         (int(self._batch_size / self.pac),),
                         fake_label,
                         dtype=torch.float,
                         device=self._device,
                     )
-
-                    print ('label_fake is {}'.format(label_fake))
+                    if i >=self._epochs-10:
+                        print ('label_fake is {}'.format(label_fake))
 
                     error_d_fake = criterion(y_fake.squeeze(), label_fake)
                     error_d_fake.backward()
@@ -398,11 +399,13 @@ class DPCTGAN(CTGANSynthesizer, Synthesizer):
                         device=self._device,
                     )
                     y_real = discriminator(real_cat)
-                    print ('y_real is {}'.format(y_real))
+                    if i >=self._epochs-10:
+                        print ('y_real is {}'.format(y_real))
                     error_d_real = criterion(y_real.squeeze(), label_true)
                     error_d_real.backward()
                     optimizerD.step()
-                    print ('real_label is {}'.format(real_label))
+                    if i >=self._epochs-10:
+                        print ('real_label is {}'.format(real_label))
 
 
                     loss_d = error_d_real + error_d_fake
